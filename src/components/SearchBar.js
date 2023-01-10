4bb566e6d01773ec7b2c3cf12edaaedb72d5ed42
Isis-Yamel <yamelamanca@gmail.com>
2023-01-10 16:28:26 -0500

    add search bar component and its set up

---
+++ src/components/SearchBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = ({term, onChangeTerm, onSubmit}) => {
+  return (
+    <View style={styles.searchWrapper}>
+      <Feather name='search' style={styles.searchIcon} />
+      <TextInput 
+        autoCapitalize='none'
+        autoCorrect={false}
+        style={styles.searchInput} 
+        placeholder='Search'
+        value={term}
+        onChangeText={onChangeTerm}
+        onEndEditing={onSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchWrapper: {
+    backgroundColor: '#E0DEDE',
+    height: 50,
+    borderRadius: 10,
+    margin: 10,
+    flexDirection: 'row',
+  },
+  searchInput: {
+    fontSize: 18,
+    flex: 1,
+  },
+  searchIcon: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15,
+  }
+})
+
+export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 
+import SearchBar from '../components/SearchBar';
+
 const SearchScreen = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <View>
+      <SearchBar 
+        term={searchTerm} 
+        onChangeTerm={term => setSearchTerm(term)}
+        onSubmit={() => console.log('END')}
+      />
       <Text>Search</Text>
     </View>
   );
